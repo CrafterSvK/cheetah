25228902ea67726e1dd71605d465937c4c4888e0
Jakub Janek <shaman.janek@gmail.com>
2019-06-30 11:29:06 +0200

    File: working state

--- framework/File.php
+++ framework/File.php
@@ -3,15 +3,33 @@ declare(strict_types=1);
 
 namespace cheetah;
 
+use Exception;
+use function file_exists;
+use function finfo_file;
+use function finfo_open;
+use function in_array;
+use function mkdir;
+use function move_uploaded_file;
+
 /**
- * File handling, doesn't work or was not tested. WIP
- * @param string directory to upload and check files in
- * @param int maximal size of a file
- * @param array formats that can be uploaded
+ * File handling
  * @author Jakub Janek
  */
 class File {
-	public function __construct(array $formats, string $directory = '/cdn', int $max_size = 5000000) {
+	private $max_size = 0;
+	private $formats = [];
+	private $directory = "";
+	private $dir;
+	private $db;
+
+	/**
+	 * File constructor.
+	 * @param array formats that can be uploaded
+	 * @param string directory to upload and check files in
+	 * @param int maximal size of a file
+	 * @throws Exception
+	 */
+	public function __construct(array $formats, string $directory = 'cdn/', int $max_size = 5000000) {
 		$this->max_size = $max_size;
 		$this->formats = $formats;
 		$this->directory = $directory;
@@ -22,44 +40,38 @@ class File {
 	/**
 	 * Upload list of files given by form
 	 * @param array var $_FILES
-	 * @return object
+	 * @return array
 	 */
-	public function upload(array $files): object {
+	public function upload(array $files): array {
+		$uploaded_files = [];
+
 		foreach ($files as $key => $file) {
 			do {
-				$name = $this->_generateFileName();
-			} while (\file_exists($this->directory . $name));
- 
-			$info = \finfo_open(FILEINFO_MIME_TYPE);
-			$type = \finfo_file($info, $file['tmp_name']);
+				$name = uniqid('');
+			} while (file_exists($this->directory . $name));
+
+			$info = finfo_open(FILEINFO_MIME_TYPE);
+			$type = finfo_file($info, $file['tmp_name']);
 
 			//Corresponding to files database table
-			$uploaded_files[$key] = (object) [
+			$uploaded_files[$key] = (object)[
 				'name' => $name,
 				'original_name' => $file['name'],
 				'directory' => $this->directory,
 				'type' => $type,
-				'size' => $file['size'] 
+				'size' => $file['size']
 			];
 
-			if (!\in_array($type, $this->formats)) $uploaded_files[$key]->error = true;
+			if (!in_array($type, $this->formats)) $uploaded_files[$key]->error = true;
 
 			//Upload, insert into database and return id into the list
-			if (\move_uploaded_file($file['tmp_name'], $this->directory . $name)
-				&& !$uploaded_files->error) {
+			if (move_uploaded_file($file['tmp_name'], $this->directory . $name)
+			&& !$uploaded_files[$key]->error) {
+				$uploaded_files[$key]->fid = $this->db->insert('file')
+					->values((array)$uploaded_files[$key])
+					->execute();
 
-				$this->db->insert('files')
-					->values((array) $uploaded_files[$key])
-					->execute(); //get inserted id is not possible with current db engine
-
-				$uploaded_files[$key]->id = $this->db->select('files')
-					->item('id')
-					->condition('name', $uploaded_files['name'])
-					->execute()
-					->result[0]['id'];
-
-
-			} else $uploaded_files->error = true;
+			} else $uploaded_files[$key]->error = true;
 		}
 
 		return $uploaded_files;
@@ -67,21 +79,20 @@ class File {
 
 	/**
 	 * Deletes file and removes it from database (Needs check)
-	 * @param int id of a file in database
+	 * @param int fid of a file in database
 	 * @return bool
 	 */
-	public function delete(int $id): bool {
-		//$file = $this->db->fetch('files', ['id', 'directory', 'name'], "id = {$id}");
-		$file = $this->db->select('files')
-			->items(['id', 'directory', 'name'])
-			->condition('id', $id)
+	public function delete(int $fid): bool {
+		$file = $this->db->select('file')
+			->items(['fid', 'directory', 'name'])
+			->condition('fid', $fid)
 			->execute()
-			->result[0];
+			->fetch_all(MYSQLI_ASSOC);
 
 		if (!unlink($file['directory'] . $file['name'])) return false;
 
-		$this->db->delete('files')
-			->condition('id', $id)
+		$this->db->delete('file')
+			->condition('fid', $fid)
 			->execute();
 
 		return true;
@@ -100,21 +111,5 @@ class File {
 		$this->dir = $dir;
 
 		return true;
-	}
-
-	/**
-	 * Generates 10 character random string of characters
-	 * @return string
-	 */
-	private function _generateFileName(): string {
-		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		$charactersLength = strlen($characters);
-		$randomString = '';
-		
-		for ($i = 0; $i < 10; $i++) {
-			$randomString .= $characters[rand(0, $charactersLength - 1)];
-		}
-
-		return $randomString;
 	}
 }
